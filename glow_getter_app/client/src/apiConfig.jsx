ea792af3611dca5ce7b37bb1c8225e9a8a415e36
corey gibson <cgibsonmm@gmail.com>
2020-03-22 16:43:34 -0400

    changed ports around

--- glow_getter_app/client/src/apiConfig.jsx
+++ glow_getter_app/client/src/apiConfig.jsx
@@ -1,65 +1,65 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://localhost:3000'
-let api
+const baseUrl = "http://localhost:3001";
+let api;
 
-if (localStorage.getItem('authToken')){
+if (localStorage.getItem("authToken")) {
   api = axios.create({
     baseURL: baseUrl,
     headers: {
-      authorization: localStorage.getItem('authToken')
+      authorization: localStorage.getItem("authToken")
     }
-  })
+  });
 } else {
   api = axios.create({
     baseURL: baseUrl
-  })
+  });
 }
 
-export const loginUser = async (loginData) => {
-  const response = await api.post('/auth/login', loginData)
-  localStorage.setItem('authToken', response.data.token);
-  api.defaults.headers.common.authorization = `Bearer ${response.data.token}`
-  return response.data.user
-}
-  
-export const registerUser = async (registerData) => {
-  const response = await api.post('/users', { user: registerData })
-  localStorage.setItem('authToken', response.data.token);
-  api.defaults.headers.common.authorization = `Bearer ${response.data.token}`
-  return response.data.user
-}
-  
+export const loginUser = async loginData => {
+  const response = await api.post("/auth/login", loginData);
+  localStorage.setItem("authToken", response.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${response.data.token}`;
+  return response.data.user;
+};
+
+export const registerUser = async registerData => {
+  const response = await api.post("/users", { user: registerData });
+  localStorage.setItem("authToken", response.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${response.data.token}`;
+  return response.data.user;
+};
+
 export const verifyUser = async () => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
   if (token) {
-    api.defaults.headers.common.authorization = `Bearer ${token}`
-    const response = await api.get('/auth/verify');
-    return response.data
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    const response = await api.get("/auth/verify");
+    return response.data;
   }
-  return false
-}
+  return false;
+};
 
 export const viewProducts = async () => {
-  const response = await api.get('/products')
-  console.log(response)
-  return response.data
-}
+  const response = await api.get("/products");
+  console.log(response);
+  return response.data;
+};
 
 export const viewCart = async () => {
-  const response = await api.get('/cart')
-  console.log(response)
-  return response.data
-}
+  const response = await api.get("/cart");
+  console.log(response);
+  return response.data;
+};
 
-export const addProductToCart = async (id) => {
-  const response = await api.post(`/cart/${id}`)
-  console.log(response)
-  return response.data 
-}
+export const addProductToCart = async id => {
+  const response = await api.post(`/cart/${id}`);
+  console.log(response);
+  return response.data;
+};
 
-export const removeProductFromCart = async (id) => {
-  const response = await api.delete(`/cart/${id}`)
-  console.log(response)
-  return(response.data)
-}
+export const removeProductFromCart = async id => {
+  const response = await api.delete(`/cart/${id}`);
+  console.log(response);
+  return response.data;
+};
